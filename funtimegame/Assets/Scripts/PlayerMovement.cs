6c8a3e01df5efee77689cc20d8c58cf4a542a35e
Kosuke Takahashi <ko@kotakahashi.com>
2020-08-03 18:26:46 -0700

    Update PlayerMovement.cs

--- funtimegame/Assets/Scripts/PlayerMovement.cs
+++ funtimegame/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 300;
+    public GameObject character;
+
+    private Rigidbody2D characterBody;
+    private float ScreenWidth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScreenWidth = Screen.width;
+        characterBody = character.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int i = 0;
+
+        while(i < Input.touchCount)
+        {
+            if(Input.GetTouch (i).position.x > ScreenWidth / 2)
+            {
+                RunCharacter(1.0f);
+            }
+            if(Input.GetTouch (i).position.x < ScreenWidth / 2)
+            {
+                RunCharacter(-1.0f);
+            }
+            ++i;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        #if UNITY_EDITOR
+        RunCharacter(Input.GetAxis("Horizontal"));
+        #endif
+    }
+
+    private void RunCharacter(float horizontalInput)
+    {
+        characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
     }
 }
