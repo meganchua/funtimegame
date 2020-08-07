dbd378cd25e2ee343bcd0b1b0c645ad7bd67ac36
meganchua <meganchua@yahoo.com>
2020-08-06 18:09:07 -0700

    getting obstacles to move

---
+++ funtimegame/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public float delay = 0.7f;
+    public float repeat = 5.0f;
+    public GameObject obstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", delay, repeat);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        Vector2 position = new Vector2(Random.Range(-2, 2), 10);
+        Instantiate(obstacles, position, Quaternion.identity);
+    }
+}
