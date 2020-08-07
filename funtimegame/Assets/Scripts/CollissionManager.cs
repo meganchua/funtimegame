73828ffeb815f9ae58167328bd4b3d6b293d80f3
Kosuke Takahashi <ko@kotakahashi.com>
2020-08-06 17:54:34 -0700

    Configured the colliders for Player
    Added a ColliderTester to test out collisions (will delete later on)

---
+++ funtimegame/Assets/Scripts/CollissionManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollissionManager : MonoBehaviour
+{
+    //public GameObject obstacle; //might need to add more depending on how many unique obstacles
+    //public GameObject point;
+    //public int pointCount++;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //GameObject e = Instantiate(explosion) as GameObject;
+        //e.transform.position = transform.position;
+
+        /*
+        if(other.gameObject.tag == "Point")
+        {
+            Debug.Log("point");
+            Destroy(other.gameObject);
+        }
+        */
+
+
+        //if(other.gameObject.tag == "Obstacle")
+        //{
+
+            Destroy(other.gameObject);
+            //this.gameObject.SetActive(false);
+            //Application.LoadLevel(Application.loadedLevel);
+
+        //}
+
+        /*
+        //point system
+        else if(other.gameObject.tag == "Point")
+        {
+            pointCount++; 
+            Destroy(other.GameObject);
+        }
+        */
+    }
+}
