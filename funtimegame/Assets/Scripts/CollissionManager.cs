ef9783d79d27a0a03657c6cb8732e34914e29492
Kosuke Takahashi <ko@kotakahashi.com>
2020-08-19 17:37:51 -0700

    Made health bar lower over time

--- funtimegame/Assets/Scripts/CollissionManager.cs
+++ funtimegame/Assets/Scripts/CollissionManager.cs
@@ -4,76 +4,33 @@ using UnityEngine;
 
 public class CollissionManager : MonoBehaviour
 {
-    //public GameObject obstacle; //might need to add more depending on how many unique obstacles
-    //public GameObject point;
-    //public int pointCount++;
-
-    //public GameOver gameOver;
-    //public Score score;
-    //public int finalScore;
-
-    public Color bluecolor;
-    public Color yellowcolor;
-
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //GameObject e = Instantiate(explosion) as GameObject;
-        //e.transform.position = transform.position;
-
-        /*
-        if(other.gameObject.tag == "Score") -OR- if(other.gameObject.CompareTag("Point"))
-        {
-            Debug.Log("point");
-            Destroy(other.gameObject);
-        }
-        */
-
-
-
         if(other.gameObject.CompareTag("Death"))
         {
-            /*
-            PlayerMovement.health = PlayerMovement.health - 10;
-            Debug.Log(PlayerMovement.health);
-            Destroy(other.gameObject);
-            */
-            PlayerMovement.health -= 1;
-            Debug.Log(PlayerMovement.health);
-            Destroy(other.gameObject);
+            // Health goes down
+            PlayerMovement.health -= 1f;
+
+            // Changes the color of player to color of what it ran into
             GetComponent<SpriteRenderer>().color = other.gameObject.GetComponent<SpriteRenderer>().color;
-            //transform.GetComponent<Renderer>().material.color = bluecolor;
-            
-            /*
-            finalScore = Score.scoreAmount;
-            gameOver.GameOverMenu(finalScore);
-            Destroy(this.gameObject);
+
             Destroy(other.gameObject);
-            */
-
-            //this.gameObject.SetActive(false);
-            //Application.LoadLevel(Application.loadedLevel);
-
         }
         else if(other.gameObject.CompareTag("Score"))
         {
+            // Score goes up
             Score.scoreAmount += 1;
-            Destroy(other.gameObject);
 
+            // Health goes up
+            if(PlayerMovement.health > 6.5f)
+                PlayerMovement.health = 7f;
+            else
+                PlayerMovement.health += 0.5f;
+
+            // Changes the color of player to color of what it ran into
             GetComponent<SpriteRenderer>().color = other.gameObject.GetComponent<SpriteRenderer>().color;
-            //this.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
-            //this.gameObject.material.SetColor("_Color", Color.red);
 
-            //transform.GetComponent<Renderer>().material.color = yellowcolor;
+            Destroy(other.gameObject);
         }
-
-        /*
-        //point system
-        else if(other.gameObject.tag == "Point")
-        {
-            pointCount++; 
-            Destroy(other.GameObject);
-        }
-        */
     }
 }
