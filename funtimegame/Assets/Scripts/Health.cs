92a5bba33a646426866abd7d0e74a8ec990da623
Kosuke Takahashi <ko@kotakahashi.com>
2020-09-23 18:05:46 -0700

    formatting c# code

--- funtimegame/Assets/Scripts/Health.cs
+++ funtimegame/Assets/Scripts/Health.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    Vector3 localScale;
-
     public GameObject front;
+    Vector3 localScale;
 
     // Start is called before the first frame update
     void Start()
